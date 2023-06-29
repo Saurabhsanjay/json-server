03a7c9489d9480c4b586ec7ffab480b009454239
Saurabh ubale <Saurabhubale371@gmail.com>
2023-06-29 19:28:42 +0530

    chagnes made

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults()
 
-
+const PORT=5001
 server.use(middlewares)
-server.use('/api', router)
-server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
+server.use(router)
+server.listen(PORT || 5000, () => {
+  console.log(`JSON Server is running on PORT ${PORT}`)
 })
 
